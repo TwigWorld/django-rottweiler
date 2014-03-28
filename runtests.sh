0ccc5f381cbbd775d93a7345c09a3976d7b4f7e9
Chris Wright <cwright@twig-world.com>
2014-03-28 11:32:48 +0000

    added coveragerc file for ci tests and modified travis to run runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,22 @@
-pushd .
-cd tests/testapp
-python manage.py test rottweiler
-popd
+#!/bin/bash
+
+args=("$@")
+
+ci=false
+
+if [ "${args[0]}" == "--ci" ]; then
+  ci=true
+fi
+
+if [ $ci == true ]; then
+  pushd .
+  cd tests/testapp
+  coverage run manage.py test rottweiler
+  coverage xml
+  popd
+else
+  pushd .
+  cd tests/testapp
+  python manage.py test rottweiler
+  popd
+fi
