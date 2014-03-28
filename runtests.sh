7f0da87cdb6de21dc9b8f5519dee20cf92b6d796
Chris Wright <cwright@twig-world.com>
2014-03-28 12:00:30 +0000

    altered runtests to allow option for viewing coverage locally

--- runtests.sh
+++ runtests.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
 args=("$@")
+index=0
 
 ci=false
+coverage=false
 
 if [ "${args[0]}" == "--ci" ]; then
   ci=true
+fi
+
+if [ "${args[0]}" == "--coverage" ]; then
+  coverage=true
 fi
 
 if [ $ci == true ]; then
   pushd .
   cd tests/testapp
   coverage run manage.py test rottweiler
-  coverage xml
   popd
+elif [ $coverage == true ]; then
+  pushd .
+  cd tests/testapp
+  coverage run manage.py test rottweiler
+  coverage html
+  open -a Google\ Chrome htmlcov/index.html
 else
   pushd .
   cd tests/testapp
