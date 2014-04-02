837bdf4084609ec8d2b224784bf61289ae9c13a1
charliequinn <charlesquinn1984@gmail.com>
2014-04-02 22:52:11 +0100

    list all urls and perms that have permission_required

--- rottweiler/urls.py
+++ rottweiler/urls.py
@@ -2,9 +2,11 @@
 
 from django.conf.urls import patterns, url
 
-from .views import ShowAllRules
+from .views import ShowAllRules, ListUrls, ShowPermission
 
 urlpatterns = patterns(
     '',
     url(r'^show-all-rules$', ShowAllRules.as_view(), name="rottweiler_show_all"),
+    url(r'^list-urls$', ListUrls.as_view(), name="list_urls"),
+    url(r'^list-urls/(?P<app_label>\w+)/(?P<codename>\w+)/$', ShowPermission.as_view(), name="list_perms"),
 )
