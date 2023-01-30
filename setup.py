1f1a997d186ced8f4399ffc6388cc36aa0619428
Arun-integrella <arun.jain@integrella.com>
2023-01-30 18:00:15 +0530

    updated setup file

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ setup(
     url='https://github.com/TwigWorld/django-rottweiler',
     long_description=README,
     author='Charlie Quinn & Chris Wright',
-    python_requires='>=2.7.0',
+    python_requires='>=3.7.0',
     install_requires=[
-        "Django>=1.11, <2.0",
-        "django-rulez@git+https://github.com/chrisglass/django-rulez.git@0f869f330a7e50aacf953265e1ce16ada16cb98a#egg=django-rulez-1.0.2",
+        "Django>=2.2, <3.2",
+        "django-rulez@git+https://github.com/TwigWorld/django-rulez.git@547b246e7531df260828381598f30ad01125b4a5#egg=django-rulez-1.0.2",
     ],
 )
